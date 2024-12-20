fb0ee86dec4fc3c82c4edf47c77a982a8969f657
Kimberley Lefebvre <klefebvre@normandiewebschool.fr>
2024-12-20 14:50:56 +0100

    finalisation du CSS et ajustement du JS

--- java-script/script.js
+++ java-script/script.js
@@ -5,4 +5,34 @@ addEventListener("DOMContentLoaded", (ev) => {
     let body = document.querySelector("body");
     body.classList.toggle("dark-mode");
   });
+
+  button = document.querySelector(".header-login-button");
+  console.log(button);
+  button.addEventListener("click", (e) => {
+    alert("To enable or disable Dark Mode, please click on the Sign Up button");
+  });
+
+  button = document.querySelector(".hero-button-register");
+  console.log(button);
+  button.addEventListener("click", (e) => {
+    alert("An error occured, please retry later...");
+  });
+
+  button = document.querySelector(".unlock-button-learn-more");
+  console.log(button);
+  button.addEventListener("click", (e) => {
+    alert("An error occured, please retry later...");
+  });
+
+  button = document.querySelector(".calender-button-learn-more");
+  console.log(button);
+  button.addEventListener("click", (e) => {
+    alert("An error occured, please retry later...");
+  });
+
+  button = document.querySelector(".footer-demo-button");
+  console.log(button);
+  button.addEventListener("click", (e) => {
+    alert("Not enough coffee to get Demo...");
+  });
 });
