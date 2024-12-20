8fb9b01d4a6f0aac1de1fccf3e95546b830e31a6
Kimberley Lefebvre <klefebvre@normandiewebschool.fr>
2024-12-20 14:28:14 +0100

    initialisation du Dark Mode

--- java-script/script.js
+++ java-script/script.js
@@ -1,1 +1,8 @@
-addEventListener("DOMContentLoaded", (ev) => {});
+addEventListener("DOMContentLoaded", (ev) => {
+  let button = document.querySelector(".header-sign-up-button");
+  console.log(button);
+  button.addEventListener("click", (e) => {
+    let body = document.querySelector("body");
+    body.classList.toggle("dark-mode");
+  });
+});
